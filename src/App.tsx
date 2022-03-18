3073ec2d9f249c386b43ca4169d1bea163f92224
Kim Young Geun <smart5265988@gmail.com>
2022-03-18 14:37:12 +0900

    trello_clone setting

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+function App() {
+  const onDragEnd = () => {};
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              <Draggable draggableId="first" index={0}>
+                {() => <li>one</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>two</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
+}
+
+export default App;
